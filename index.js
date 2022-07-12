0f2573c751ad472b3ecb9d8c196b24e8189a0a8d
Ciaran98 <ciaran.melarkey@hotmail.com>
2022-07-12 22:19:25 +0100

    Heroku Fixes

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 app = express();
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
-// this * route is to serve project on different page routes except root `/`
+app.use(serveStatic(path.join(__dirname, "dist")));
 const port = process.env.PORT || 3000;
 app.listen(port);
 
