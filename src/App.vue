f005f5def39f1f5f07d49ef8dd06bfc89d781608
Ciaran98 <ciaran.melarkey@hotmail.com>
2022-07-14 10:40:44 +0100

    Changed header to moving gradient

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,84 @@
 <template>
-  <h1 class="header">Vinyle.</h1>
+  <h1 class="header" id="gradient">Vinyle.</h1>
   <router-view />
 </template>
+<script>
+import $ from "jquery";
+let colors = new Array(
+  [62, 35, 255],
+  [60, 255, 60],
+  [255, 35, 98],
+  [45, 175, 230],
+  [255, 0, 255],
+  [255, 128, 0]
+);
 
+let step = 0;
+//color table indices for:
+// current color left
+// next color left
+// current color right
+// next color right
+let colorIndices = [0, 1, 2, 3];
+
+//transition speed
+let gradientSpeed = 0.002;
+
+function updateGradient() {
+  if ($ === undefined) return;
+
+  let c0_0 = colors[colorIndices[0]];
+  let c0_1 = colors[colorIndices[1]];
+  let c1_0 = colors[colorIndices[2]];
+  let c1_1 = colors[colorIndices[3]];
+
+  let istep = 1 - step;
+  let r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+  let g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+  let b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+  let color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
+
+  let r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+  let g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+  let b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  let color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
+
+  $("#gradient")
+    .css({
+      background:
+        "-webkit-gradient(linear, left top, right top, from(" +
+        color1 +
+        "), to(" +
+        color2 +
+        "))",
+    })
+    .css({
+      background:
+        "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)",
+      "-webkit-background-clip": "text",
+      "-webkit-text-fill-color": "transparent",
+    });
+
+  step += gradientSpeed;
+  if (step >= 1) {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] =
+      (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) %
+      colors.length;
+    colorIndices[3] =
+      (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) %
+      colors.length;
+  }
+}
+
+setInterval(updateGradient, 10);
+export default {};
+</script>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Raleway&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap");
@@ -15,13 +91,6 @@ h1.header {
   margin: 0;
   font-family: "Dancing Script", cursive;
   font-size: 200px;
-  background: linear-gradient(
-    90deg,
-    rgb(222, 235, 47) 0%,
-    rgb(40, 223, 255) 100%
-  );
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
